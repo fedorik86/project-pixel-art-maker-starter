d21fbdf9ea6931dd196164e987ff066846c3bf60
fedorik86 <70436582+fedorik86@users.noreply.github.com>
2020-08-29 14:02:38 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
-// Select color input
-// Select size input
+// Function to draw a grid
+function makeGrid(evt) {
+  	evt.preventDefault();
+  	// Define variables
+  	const table = document.querySelector('#pixelCanvas');
+  	const height = document.querySelector('#inputHeight').value;
+  	const width = document.querySelector('#inputWidth').value;
+  	// Remove grid if already exists, also removes event listerers for grid cells
+  	if (table.rows.length !== 0) {
+  		tableContents = table.querySelector('tbody');
+  		tableContents.remove();
+  	};
+  	// Create a grid
+ 	for (let x = 0; x < height; x++) {
+		let row = table.insertRow(x);
+		for (let y = 0; y < width; y++) {
+			let column = row.insertCell(y);
+		};
+	};
+	// Select all cells and add event listener to each calling inline colorCell function
+	const cells = document.querySelectorAll('td');
+		for (let c = 0; c < cells.length; c++) {
+  		cells[c].addEventListener('click', function colorCell(evt) {
+  			evt.target.bgColor = document.querySelector('#colorPicker').value;
+		});
+	};
+};
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}
+// Select submit button and add event listener to it calling makeGrid function
+const clickSubmit = document.querySelector('#sizePicker');
+clickSubmit.addEventListener('submit', makeGrid);
